fbc79841b2a8a0aaf79a5b2871a52807752c779d
SERJCOM <sergeylukin228@yandex.ru>
2022-11-28 15:28:03 +0200

    first commit to the project

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -2,3 +2,29 @@
 // Напишите ответ здесь:
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
+
+
+#include <iostream>
+
+using namespace std;
+
+bool sod(int a){
+    bool b = false;
+    while(a > 0){
+        if(a % 10 == 3) {
+            b = true;
+            break;
+        }
+        a = a / 10;
+    }
+    return b;
+}
+
+int main()
+{
+    int count = 0;
+    for(int i = 0; i < 1000; i++)
+        if(sod(i)) count ++;
+    cout << count;
+    return 0;
+}
