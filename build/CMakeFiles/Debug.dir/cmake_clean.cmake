0e8018967f810d9bb5ef7579fbfa4dfeff84e573
SERJCOM <sergeylukin228@yandex.ru>
2023-04-12 23:18:21 +0300

    commit

---
+++ build/CMakeFiles/Debug.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Debug.dir/document.cpp.o"
+  "CMakeFiles/Debug.dir/document.cpp.o.d"
+  "CMakeFiles/Debug.dir/main.cpp.o"
+  "CMakeFiles/Debug.dir/main.cpp.o.d"
+  "CMakeFiles/Debug.dir/read_input_functions.cpp.o"
+  "CMakeFiles/Debug.dir/read_input_functions.cpp.o.d"
+  "CMakeFiles/Debug.dir/remove_duplicates.cpp.o"
+  "CMakeFiles/Debug.dir/remove_duplicates.cpp.o.d"
+  "CMakeFiles/Debug.dir/request_queue.cpp.o"
+  "CMakeFiles/Debug.dir/request_queue.cpp.o.d"
+  "CMakeFiles/Debug.dir/search_server.cpp.o"
+  "CMakeFiles/Debug.dir/search_server.cpp.o.d"
+  "CMakeFiles/Debug.dir/string_processing.cpp.o"
+  "CMakeFiles/Debug.dir/string_processing.cpp.o.d"
+  "Debug"
+  "Debug.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
